758196a37d6523205e9b6f274e9b48992464dbe4
Ankit Vashisth <dryisholeup@gmail.com>
2024-05-31 21:35:27 +0530

    fancy pattern

--- patterns.cpp
+++ patterns.cpp
@@ -65,38 +65,52 @@ cin>>n;
         
     //     cout << endl;
 
-    //hollow diamond
-    for(int row=0;row<n;row++){
-        for(int col=0;col<n-row;col++){
-            cout<<"*";
-        }
-        for(int col=0;col<2*row+1;col++){
-            cout<<" ";
-        }
-            for(int col=0;col<n-row;col++){
-            cout<<"*";
+    // //hollow diamond
+    // for(int row=0;row<n;row++){
+    //     for(int col=0;col<n-row;col++){
+    //         cout<<"*";
+    //     }
+    //     for(int col=0;col<2*row+1;col++){
+    //         cout<<" ";
+    //     }
+    //         for(int col=0;col<n-row;col++){
+    //         cout<<"*";
         
-        }
+    //     }
 
       
-        cout<<"\n";
-    }
-    for(int row=0;row<n;row++){
-        for(int col=0;col<row+1;col++){
-            cout<<"*";
-        }
-        for(int col=0;col<2*n-2*row-1;col++){
-            cout<<" ";
-        }
-            for(int col=0;col<row+1;col++){
-            cout<<"*";
+    //     cout<<"\n";
+    // }
+    // for(int row=0;row<n;row++){
+    //     for(int col=0;col<row+1;col++){
+    //         cout<<"*";
+    //     }
+    //     for(int col=0;col<2*n-2*row-1;col++){
+    //         cout<<" ";
+    //     }
+    //         for(int col=0;col<row+1;col++){
+    //         cout<<"*";
         
-        }
+    //     }
         
       
-        cout<<"\n";
-    }
+    //     cout<<"\n";
+    // }
 
+
+    //fancy pattern
+
+    // for(int row=0;row<n;row++){
+    //     for(int col=0;col<row+1;col++){
+    //        cout<<row+1;
+    //         if(row!=col){
+    //             cout<<"*";
+    //         }
+    //     }
+    //     cout<<"\n";
+    // }
+
+    
 
     return 0;
 
